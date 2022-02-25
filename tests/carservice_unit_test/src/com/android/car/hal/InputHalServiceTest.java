LineageOS/android_packages_services_Car

Remove hal service constraints on custom input events
Bug: 217288618
Test: atest InputHalServiceTest
Change-Id: I3408f0da076f2163ebf4c3655e80e743ab1fc869

tests/carservice_unit_test/src/com/android/car/hal/InputHalServiceTest.java
@@ -589,20 +589,36 @@ public class InputHalServiceTest {
         mInputHalService.onHalEvents(List.of(v));
 
         verify(mInputListener, never()).onCustomInputEvent(any());
+    }
 
-        // Add repeatCounter and set invalid input code.
-        v = mPropValueBuilder.build(VehicleProperty.HW_CUSTOM_INPUT, /* areaId= */ 0,
-                new int[]{CustomInputEvent.INPUT_CODE_F1 - 1,
-                        CarOccupantZoneManager.DISPLAY_TYPE_INSTRUMENT_CLUSTER, 1});
-        mInputHalService.onHalEvents(List.of(v));
-        verify(mInputListener, never()).onCustomInputEvent(any());
+    @Test
+    public void dispatchesCustomInputEvent_acceptInputCodeHigherThanF10() {
+        // Custom Input Events may accept input code values outside the
+        // CUSTOM_EVENT_F1 to F10 range.
+        int someInputCodeValueHigherThanF10 = 1000;
 
-        // Set invalid input code.
-        v = mPropValueBuilder.build(VehicleProperty.HW_CUSTOM_INPUT, /* areaId= */ 0,
-                new int[]{CustomInputEvent.INPUT_CODE_F10 + 1,
-                        CarOccupantZoneManager.DISPLAY_TYPE_INSTRUMENT_CLUSTER, 1});
-        mInputHalService.onHalEvents(List.of(v));
-        verify(mInputListener, never()).onCustomInputEvent(any());
+        // Arrange mInputListener to capture incoming CustomInputEvent
+        subscribeListener();
+
+        List<CustomInputEvent> events = new ArrayList<>();
+        doAnswer(invocation -> {
+            CustomInputEvent event = invocation.getArgument(0);
+            events.add(event);
+            return null;
+        }).when(mInputListener).onCustomInputEvent(any());
+
+        // Arrange
+        int repeatCounter = 1;
+        HalPropValue customInputPropValue = makeCustomInputPropValue(
+                someInputCodeValueHigherThanF10, VehicleDisplay.MAIN, repeatCounter);
+
+        // Act
+        mInputHalService.onHalEvents(List.of(customInputPropValue));
+
+        // Assert
+        assertThat(events).containsExactly(new CustomInputEvent(
+                someInputCodeValueHigherThanF10, CarOccupantZoneManager.DISPLAY_TYPE_MAIN,
+                repeatCounter));
     }
 
     private void subscribeListener() {

bluestacks/packages-services-Car-a16

Bug: 213896131
Change-Id: I351165ac44d11956f3e12a788355f91a573f32c8
Merged-In: I3408f0da076f2163ebf4c3655e80e743ab1fc869
(cherry picked from commit 2c9683a2c9e3363f4e8cbd28a4b56737b196a8a0)

tests/carservice_unit_test/src/com/android/car/hal/InputHalServiceTest.java
@@ -487,6 +487,36 @@ public class InputHalServiceTest {
                 repeatCounter));
     }
 
+    @Test
+    public void dispatchesCustomInputEvent_acceptInputCodeHigherThanF10() {
+        // Custom Input Events may accept input code values outside the
+        // CUSTOM_EVENT_F1 to F10 range.
+        int someInputCodeValueHigherThanF10 = 1000;
+
+        // Arrange mInputListener to capture incoming CustomInputEvent
+        subscribeListener();
+
+        List<CustomInputEvent> events = new ArrayList<>();
+        doAnswer(invocation -> {
+            CustomInputEvent event = invocation.getArgument(0);
+            events.add(event);
+            return null;
+        }).when(mInputListener).onCustomInputEvent(any());
+
+        // Arrange
+        int repeatCounter = 1;
+        VehiclePropValue customInputPropValue = makeCustomInputPropValue(
+                someInputCodeValueHigherThanF10, VehicleDisplay.MAIN, repeatCounter);
+
+        // Act
+        mInputHalService.onHalEvents(ImmutableList.of(customInputPropValue));
+
+        // Assert
+        assertThat(events).containsExactly(new CustomInputEvent(
+                CustomInputEvent.INPUT_CODE_F1, CarOccupantZoneManager.DISPLAY_TYPE_MAIN,
+                repeatCounter));
+    }
+
     private void subscribeListener() {
         mInputHalService.takeProperties(ImmutableSet.of(HW_KEY_INPUT_CONFIG));
         assertThat(mInputHalService.isKeyInputSupported()).isTrue();