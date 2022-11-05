LineageOS/android_packages_services_Car

Initial implementation for OEM plug-in Service
CTS-Coverage-Bug: 245982224
Bug: 226406223
Test: m(successful builds), Unit tests to be added in followup CL
Change-Id: If1c10a1f3e49171e24f3912dd042da1e93054ba2

car-lib/src/android/car/oem/OemCarAudioFocusService.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.NonNull;
+import android.annotation.SystemApi;
+import android.car.annotation.ApiRequirements;
+import android.media.AudioFocusInfo;
+
+import java.util.List;
+
+/*
+ * OemCarAudioFocusServiceInterface would expose all the method from IOemCarAudioFocusService. It
+ * should always be in sync with IOemCarAudioFocusService. Oem will implement
+ * OemCarAudioFocusServiceInterface which would be used by OemCarAudioFocusService.
+ */
+/**
+ * Interface for Audio focus for OEM Service.
+ *
+ * @hide
+ */
+@SystemApi
+public interface OemCarAudioFocusService extends OemCarServiceComponent {
+    /**
+     * Updates audio focus change. It is one way call for OEM Service.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
+            @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId);
+}

car-lib/src/android/car/oem/OemCarAudioFocusServiceImpl.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.NonNull;
+import android.car.annotation.ApiRequirements;
+import android.media.AudioFocusInfo;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * This code will be running as part of the OEM Service. This implements
+ * {@code IOemCarAudioFocusService} as hidden class. It is exposed as IBinder from {@link
+ * OemCarService#getOemAudioFocusService()}
+ *
+ * @hide
+ */
+final class OemCarAudioFocusServiceImpl extends IOemCarAudioFocusService.Stub
+        implements OemCarServiceComponent {
+
+    private final OemCarAudioFocusService mOemCarAudioFocusService;
+
+    OemCarAudioFocusServiceImpl(
+            @NonNull OemCarAudioFocusService oemCarAudioFocusService) {
+        mOemCarAudioFocusService = oemCarAudioFocusService;
+    }
+
+    @Override
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    public void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
+            @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId) {
+        mOemCarAudioFocusService.audioFocusChanged(currentFocusHolders, currentFocusLosers,
+                zoneId);
+    }
+
+    @Override
+    public void init() {
+        mOemCarAudioFocusService.init();
+    }
+
+    @Override
+    public void release() {
+        mOemCarAudioFocusService.release();
+    }
+
+    @Override
+    public void dump(PrintWriter writer, String[] args) {
+        mOemCarAudioFocusService.dump(writer, args);
+    }
+
+    @Override
+    public void onCarServiceReady() {
+        mOemCarAudioFocusService.onCarServiceReady();
+    }
+}

car-lib/src/android/car/oem/OemCarServiceComponent.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.Nullable;
+import android.annotation.SystemApi;
+import android.car.annotation.ApiRequirements;
+
+import java.io.PrintWriter;
+
+/**
+ * Contains life cycle methods for the OEM Service.
+ *
+ * <p>This is to enforce structure on the OEM Service components. {link OemCarService} would call
+ * these methods.
+ *
+ * @hide
+ */
+@SystemApi
+@SuppressWarnings("[NotCloseable]")
+public interface OemCarServiceComponent {
+    /**
+     * Initializes required resources.
+     *
+     * <p>This is called for each service component during {@link OemCarService#onCreate()}
+     * call of OemCarService.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void init();
+
+    /**
+     * Releases required resources.
+     *
+     * <p>This is called for each service component during {@link OemCarService#onDestroy()}
+     * call of OemCarService.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void release();
+
+    /**
+     * Dumps the service component details.
+     *
+     * <p>Each service component should implement a dump command to dump. It is called from
+     * {@link OemCarService#dump(java.io.FileDescriptor, PrintWriter, String[])} call.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void dump(@Nullable PrintWriter writer, @Nullable String[] args);
+
+    /**
+     * Informs if CarService is ready.
+     *
+     * <p> Each service component should do the necessary initialization depending on CarService. It
+     * is called from {@link OemCarService#onCarServiceReady()} call.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void onCarServiceReady();
+}

bluestacks/packages-services-Car-a16

- cherry pick from master brnch
- fixed merged conflict
- changed minCarVersion
Merged-In: If1c10a1f3e49171e24f3912dd042da1e93054ba2
(cherry picked from commit 3a697600c1e648e1282c51669a5ea3aaf701fff0)

car-lib/src/android/car/oem/OemCarAudioFocusService.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.NonNull;
+import android.annotation.SystemApi;
+import android.car.annotation.ApiRequirements;
+import android.media.AudioFocusInfo;
+
+import java.util.List;
+
+/*
+ * OemCarAudioFocusServiceInterface would expose all the method from IOemCarAudioFocusService. It
+ * should always be in sync with IOemCarAudioFocusService. Oem will implement
+ * OemCarAudioFocusServiceInterface which would be used by OemCarAudioFocusService.
+ */
+/**
+ * Interface for Audio focus for OEM Service.
+ *
+ * @hide
+ */
+@SystemApi
+public interface OemCarAudioFocusService extends OemCarServiceComponent {
+    /**
+     * Updates audio focus change. It is one way call for OEM Service.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
+            @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId);
+}

car-lib/src/android/car/oem/OemCarAudioFocusServiceImpl.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.NonNull;
+import android.car.annotation.ApiRequirements;
+import android.media.AudioFocusInfo;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * This code will be running as part of the OEM Service. This implements
+ * {@code IOemCarAudioFocusService} as hidden class. It is exposed as IBinder from {@link
+ * OemCarService#getOemAudioFocusService()}
+ *
+ * @hide
+ */
+final class OemCarAudioFocusServiceImpl extends IOemCarAudioFocusService.Stub
+        implements OemCarServiceComponent {
+
+    private final OemCarAudioFocusService mOemCarAudioFocusService;
+
+    OemCarAudioFocusServiceImpl(
+            @NonNull OemCarAudioFocusService oemCarAudioFocusService) {
+        mOemCarAudioFocusService = oemCarAudioFocusService;
+    }
+
+    @Override
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    public void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
+            @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId) {
+        mOemCarAudioFocusService.audioFocusChanged(currentFocusHolders, currentFocusLosers,
+                zoneId);
+    }
+
+    @Override
+    public void init() {
+        mOemCarAudioFocusService.init();
+    }
+
+    @Override
+    public void release() {
+        mOemCarAudioFocusService.release();
+    }
+
+    @Override
+    public void dump(PrintWriter writer, String[] args) {
+        mOemCarAudioFocusService.dump(writer, args);
+    }
+
+    @Override
+    public void onCarServiceReady() {
+        mOemCarAudioFocusService.onCarServiceReady();
+    }
+}

car-lib/src/android/car/oem/OemCarServiceComponent.java
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.car.oem;
+
+import android.annotation.Nullable;
+import android.annotation.SystemApi;
+import android.car.annotation.ApiRequirements;
+
+import java.io.PrintWriter;
+
+/**
+ * Contains life cycle methods for the OEM Service.
+ *
+ * <p>This is to enforce structure on the OEM Service components. {link OemCarService} would call
+ * these methods.
+ *
+ * @hide
+ */
+@SystemApi
+@SuppressWarnings("[NotCloseable]")
+public interface OemCarServiceComponent {
+    /**
+     * Initializes required resources.
+     *
+     * <p>This is called for each service component during {@link OemCarService#onCreate()}
+     * call of OemCarService.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void init();
+
+    /**
+     * Releases required resources.
+     *
+     * <p>This is called for each service component during {@link OemCarService#onDestroy()}
+     * call of OemCarService.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void release();
+
+    /**
+     * Dumps the service component details.
+     *
+     * <p>Each service component should implement a dump command to dump. It is called from
+     * {@link OemCarService#dump(java.io.FileDescriptor, PrintWriter, String[])} call.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void dump(@Nullable PrintWriter writer, @Nullable String[] args);
+
+    /**
+     * Informs if CarService is ready.
+     *
+     * <p> Each service component should do the necessary initialization depending on CarService. It
+     * is called from {@link OemCarService#onCarServiceReady()} call.
+     */
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    void onCarServiceReady();
+}