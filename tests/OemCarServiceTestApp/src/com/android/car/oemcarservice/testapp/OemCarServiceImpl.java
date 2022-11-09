LineageOS/android_packages_services_Car

Added a test app for OEM plug-in Service
It is required for testing purpose.
Step to add test app as the OEM Service app:
- build and install OemCarServiceTestApp
- adb root
- adb shell setprop com.android.car.internal.debug.oem_car_service
com.android.car.oemcarservice.testapp/.OemCarServiceImpl
- adb shell stop
- adb shell start
- adb shell getprop com.android.car.internal.debug.oem_car_service
- Confirm property is set

OemCarServiceTestApp would be running as OEM Car Service

Bug: 239705430
Test: m
Change-Id: I33158fabc23982ea7e09f5d5ed9cf28e71d7591a

tests/OemCarServiceTestApp/src/com/android/car/oemcarservice/testapp/OemCarServiceImpl.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.oemcarservice.testapp;
+
+import android.car.CarVersion;
+import android.car.oem.OemCarAudioFocusService;
+import android.car.oem.OemCarService;
+import android.util.Log;
+
+import com.android.internal.annotations.GuardedBy;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
+public final class OemCarServiceImpl extends OemCarService {
+
+    private static final String TAG = OemCarServiceImpl.class.getSimpleName();
+    private static final CarVersion SUPPORTED_CAR_VERSION =
+            CarVersion.VERSION_CODES.UPSIDE_DOWN_CAKE_0;
+
+    private final Object mLock = new Object();
+
+    @GuardedBy("mLock")
+    private OemCarAudioFocusServiceImpl mOemCarAudioFocusServiceImpl;
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+
+        // Initialize all subcomponents.
+        synchronized (mLock) {
+            mOemCarAudioFocusServiceImpl = new OemCarAudioFocusServiceImpl();
+        }
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        // Releases resource from subcomponents.
+        synchronized (mLock) {
+            mOemCarAudioFocusServiceImpl = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
+        Log.d(TAG, "dump");
+        writer.println("Dump OemCarServiceImpl");
+        writer.println("SUPPORTED_CAR_VERSION:" + SUPPORTED_CAR_VERSION);
+
+    }
+
+    @Override
+    public OemCarAudioFocusService getOemAudioFocusService() {
+        synchronized (mLock) {
+            Log.d(TAG, "getOemAudioFocusService returned " + mOemCarAudioFocusServiceImpl);
+            return mOemCarAudioFocusServiceImpl;
+        }
+    }
+
+
+    @Override
+    public boolean isOemServiceReady() {
+        Log.d(TAG, "isOemServiceReady");
+        return true;
+    }
+
+    @Override
+    public void onCarServiceReady() {
+        super.onCarServiceReady();
+        Log.d(TAG, "onCarServiceReady");
+    }
+
+    @Override
+    public CarVersion getSupportedCarVersion() {
+        Log.d(TAG, "OemCarServiceImpl getSupportedCarVersion called");
+        return SUPPORTED_CAR_VERSION;
+    }
+
+}

bluestacks/packages-services-Car-a16

Merged-In: I33158fabc23982ea7e09f5d5ed9cf28e71d7591a
(cherry picked from commit f79ca464849206ee22fe08b4e91684659094fd61)

tests/OemCarServiceTestApp/src/com/android/car/oemcarservice/testapp/OemCarServiceImpl.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.oemcarservice.testapp;
+
+import android.car.CarVersion;
+import android.car.oem.OemCarAudioFocusService;
+import android.car.oem.OemCarService;
+import android.util.Log;
+
+import com.android.internal.annotations.GuardedBy;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
+public final class OemCarServiceImpl extends OemCarService {
+
+    private static final String TAG = OemCarServiceImpl.class.getSimpleName();
+    private static final CarVersion SUPPORTED_CAR_VERSION =
+            CarVersion.VERSION_CODES.TIRAMISU_2;
+
+    private final Object mLock = new Object();
+
+    @GuardedBy("mLock")
+    private OemCarAudioFocusServiceImpl mOemCarAudioFocusServiceImpl;
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+
+        // Initialize all subcomponents.
+        synchronized (mLock) {
+            mOemCarAudioFocusServiceImpl = new OemCarAudioFocusServiceImpl();
+        }
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        // Releases resource from subcomponents.
+        synchronized (mLock) {
+            mOemCarAudioFocusServiceImpl = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
+        Log.d(TAG, "dump");
+        writer.println("Dump OemCarServiceImpl");
+        writer.println("SUPPORTED_CAR_VERSION:" + SUPPORTED_CAR_VERSION);
+
+    }
+
+    @Override
+    public OemCarAudioFocusService getOemAudioFocusService() {
+        synchronized (mLock) {
+            Log.d(TAG, "getOemAudioFocusService returned " + mOemCarAudioFocusServiceImpl);
+            return mOemCarAudioFocusServiceImpl;
+        }
+    }
+
+
+    @Override
+    public boolean isOemServiceReady() {
+        Log.d(TAG, "isOemServiceReady");
+        return true;
+    }
+
+    @Override
+    public void onCarServiceReady() {
+        super.onCarServiceReady();
+        Log.d(TAG, "onCarServiceReady");
+    }
+
+    @Override
+    public CarVersion getSupportedCarVersion() {
+        Log.d(TAG, "OemCarServiceImpl getSupportedCarVersion called");
+        return SUPPORTED_CAR_VERSION;
+    }
+
+}