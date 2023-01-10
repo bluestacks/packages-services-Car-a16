LineageOS/android_packages_services_Car

Updated car volume info with current volume state
Added the gain, mute, blocked, and attenuated state to the car volume
info. Also added APIs to query this information from car audio manager.
Also added test for the APIs, the cts were added to a different CL.

Bug: 240615622
Test: atest CarAudioManagerTest com.android.car.audio
Test: atest CarVolumeInfoUnitTest
Change-Id: Ibb8bb8e266a5658acdd28abe602ab30bdcf94dd8

tests/android_car_api_test/src/android/car/apitest/media/CarAudioManagerTest.java
@@ -28,6 +28,7 @@ import static org.junit.Assume.assumeTrue;
 
 import android.car.apitest.CarApiTestBase;
 import android.car.media.CarAudioManager;
+import android.car.media.CarVolumeGroupInfo;
 import android.car.test.ApiCheckerRule.Builder;
 import android.media.AudioDeviceInfo;
 import android.os.Process;
@@ -61,6 +62,41 @@ public final class CarAudioManagerTest extends CarApiTestBase {
     public void setUp() throws Exception {
         mCarAudioManager = (CarAudioManager) getCar().getCarManager(AUDIO_SERVICE);
         assertThat(mCarAudioManager).isNotNull();
+    }
+
+    @Test
+    public void getVolumeGroupInfo() {
+        assumeDynamicRoutingIsEnabled();
+
+        int groupCount = mCarAudioManager.getVolumeGroupCount(PRIMARY_AUDIO_ZONE);
+
+        for (int index = 0; index < groupCount; index++) {
+            CarVolumeGroupInfo info =
+                    mCarAudioManager.getVolumeGroupInfo(PRIMARY_AUDIO_ZONE, index);
+            expectWithMessage("Car volume group info id for group %s", index)
+                    .that(info.getId()).isEqualTo(index);
+            expectWithMessage("Car volume group info zone for group %s", index)
+                    .that(info.getZoneId()).isEqualTo(PRIMARY_AUDIO_ZONE);
+        }
+    }
+
+    @Test
+    public void getVolumeGroupInfosForZone() {
+        assumeDynamicRoutingIsEnabled();
+
+        int groupCount = mCarAudioManager.getVolumeGroupCount(PRIMARY_AUDIO_ZONE);
+
+        List<CarVolumeGroupInfo> infos =
+                mCarAudioManager.getVolumeGroupInfosForZone(PRIMARY_AUDIO_ZONE);
+
+        expectWithMessage("Car volume group infos for primary zone")
+                .that(infos).hasSize(groupCount);
+        for (int index = 0; index < groupCount; index++) {
+            CarVolumeGroupInfo info =
+                    mCarAudioManager.getVolumeGroupInfo(PRIMARY_AUDIO_ZONE, index);
+            expectWithMessage("Car volume group infos for primary zone and group %s", index)
+                    .that(infos).contains(info);
+        }
     }
 
     @Test

bluestacks/packages-services-Car-a16

Merged-In: Ibb8bb8e266a5658acdd28abe602ab30bdcf94dd8
(cherry picked from commit 8eb3383da21c1ba2d35b4b7f6e7136a28e5edd01)

tests/android_car_api_test/src/android/car/apitest/media/CarAudioManagerTest.java
@@ -23,11 +23,13 @@ import static android.car.media.CarAudioManager.PRIMARY_AUDIO_ZONE;
 import static android.media.AudioAttributes.USAGE_MEDIA;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.junit.Assume.assumeTrue;
 
 import android.car.apitest.CarApiTestBase;
 import android.car.media.CarAudioManager;
+import android.car.media.CarVolumeGroupInfo;
 import android.media.AudioDeviceInfo;
 import android.os.Process;
 
@@ -50,6 +52,41 @@ public class CarAudioManagerTest extends CarApiTestBase {
     public void setUp() throws Exception {
         mCarAudioManager = (CarAudioManager) getCar().getCarManager(AUDIO_SERVICE);
         assertThat(mCarAudioManager).isNotNull();
+    }
+
+    @Test
+    public void getVolumeGroupInfo() {
+        assumeDynamicRoutingIsEnabled();
+
+        int groupCount = mCarAudioManager.getVolumeGroupCount(PRIMARY_AUDIO_ZONE);
+
+        for (int index = 0; index < groupCount; index++) {
+            CarVolumeGroupInfo info =
+                    mCarAudioManager.getVolumeGroupInfo(PRIMARY_AUDIO_ZONE, index);
+            assertWithMessage("Car volume group info id for group %s", index)
+                    .that(info.getId()).isEqualTo(index);
+            assertWithMessage("Car volume group info zone for group %s", index)
+                    .that(info.getZoneId()).isEqualTo(PRIMARY_AUDIO_ZONE);
+        }
+    }
+
+    @Test
+    public void getVolumeGroupInfosForZone() {
+        assumeDynamicRoutingIsEnabled();
+
+        int groupCount = mCarAudioManager.getVolumeGroupCount(PRIMARY_AUDIO_ZONE);
+
+        List<CarVolumeGroupInfo> infos =
+                mCarAudioManager.getVolumeGroupInfosForZone(PRIMARY_AUDIO_ZONE);
+
+        assertWithMessage("Car volume group infos for primary zone")
+                .that(infos).hasSize(groupCount);
+        for (int index = 0; index < groupCount; index++) {
+            CarVolumeGroupInfo info =
+                    mCarAudioManager.getVolumeGroupInfo(PRIMARY_AUDIO_ZONE, index);
+            assertWithMessage("Car volume group infos for primary zone and group %s", index)
+                    .that(infos).contains(info);
+        }
     }
 
     @Test