LineageOS/android_packages_services_Car

Added API to query volume group audio attributes
The API can be used to determine which audio attributes are assigned to
a particular volume group.

Bug: 240615622
Test: atest com.android.car.audio
Test: atest CarAudioManagerTest
Change-Id: Ief86d5dc8bc60add9d329e4f5383b628797793a2

car-lib/src/android/car/media/CarAudioManager.java
@@ -595,6 +595,30 @@ public final class CarAudioManager extends CarManagerBase {
     }
 
     /**
+     * Returns a list of audio attributes associated with the volume group info.
+     *
+     * @param groupInfo group info to query
+     * @throws NullPointerException if the volume group info is {@code null}
+     *
+     * @return list of audio attributes associated with the volume group info
+     *
+     * @hide
+     */
+    @SystemApi
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @RequiresPermission(Car.PERMISSION_CAR_CONTROL_AUDIO_VOLUME)
+    @NonNull
+    public List<AudioAttributes> getAudioAttributesForVolumeGroup(
+            @NonNull CarVolumeGroupInfo groupInfo) {
+        try {
+            return mService.getAudioAttributesForVolumeGroup(groupInfo);
+        } catch (RemoteException e) {
+            return handleRemoteExceptionFromCarService(e, Collections.EMPTY_LIST);
+        }
+    }
+
+    /**
      * Gets array of {@link AudioAttributes} usages for a volume group in a zone.
      *
      * @param zoneId The zone id whose volume group is queried.

bluestacks/packages-services-Car-a16

Merged-In: Ief86d5dc8bc60add9d329e4f5383b628797793a2
(cherry picked from commit 44ce6457d6844a11e09184f72680cb857783b091)

car-lib/src/android/car/media/CarAudioManager.java
@@ -591,6 +591,30 @@ public final class CarAudioManager extends CarManagerBase {
     }
 
     /**
+     * Returns a list of audio attributes associated with the volume group info.
+     *
+     * @param groupInfo group info to query
+     * @throws NullPointerException if the volume group info is {@code null}
+     *
+     * @return list of audio attributes associated with the volume group info
+     *
+     * @hide
+     */
+    @SystemApi
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @RequiresPermission(Car.PERMISSION_CAR_CONTROL_AUDIO_VOLUME)
+    @NonNull
+    public List<AudioAttributes> getAudioAttributesForVolumeGroup(
+            @NonNull CarVolumeGroupInfo groupInfo) {
+        try {
+            return mService.getAudioAttributesForVolumeGroup(groupInfo);
+        } catch (RemoteException e) {
+            return handleRemoteExceptionFromCarService(e, Collections.EMPTY_LIST);
+        }
+    }
+
+    /**
      * Gets array of {@link AudioAttributes} usages for a volume group in a zone.
      *
      * @param zoneId The zone id whose volume group is queried.