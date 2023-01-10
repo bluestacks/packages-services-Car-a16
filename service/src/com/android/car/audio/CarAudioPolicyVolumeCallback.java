LineageOS/android_packages_services_Car

Added test OEM car ducking service
Also added the test OEM car volume service. Needed to modify the
current car audio service volume stack to use the OEM ducking and volume
services.

Bug: 240615622
Test: atest CarAudioManagerTest com.android.car.audio
Change-Id: Ia158c41525d7b58d37b23f012c45e132d0da5e9f

service/src/com/android/car/audio/CarAudioPolicyVolumeCallback.java
@@ -27,20 +27,30 @@ import static android.media.AudioManager.ADJUST_TOGGLE_MUTE;
 import static android.media.AudioManager.ADJUST_UNMUTE;
 import static android.media.AudioManager.FLAG_FROM_KEY;
 import static android.media.AudioManager.FLAG_SHOW_UI;
-import static android.util.Log.DEBUG;
 import static android.util.Log.VERBOSE;
 
 import static com.android.car.CarLog.TAG_AUDIO;
 
 import android.car.builtin.util.Slogf;
+import android.car.media.CarVolumeGroupInfo;
+import android.car.oem.OemCarAudioVolumeRequest;
+import android.car.oem.OemCarVolumeChangeInfo;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.audiopolicy.AudioPolicy;
+import android.util.Log;
 
+import com.android.car.CarLocalServices;
+import com.android.car.oem.CarOemProxyService;
 import com.android.internal.annotations.VisibleForTesting;
 
+import java.util.List;
 import java.util.Objects;
 
 final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCallback {
+
+    public static final boolean DEBUG = Slogf.isLoggable(TAG_AUDIO, Log.DEBUG);
+
     private final AudioManager mAudioManager;
     private final boolean mUseCarVolumeGroupMuting;
     private final AudioPolicyVolumeCallbackInternal mVolumeCallback;
@@ -50,7 +60,7 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
             CarAudioPolicyVolumeCallback callback) {
         Objects.requireNonNull(policyBuilder, "AudioPolicy.Builder cannot be null");
         policyBuilder.setAudioPolicyVolumeCallback(callback);
-        if (Slogf.isLoggable(TAG_AUDIO, DEBUG)) {
+        if (DEBUG) {
             Slogf.d(TAG_AUDIO, "Registered car audio policy volume callback");
         }
     }
@@ -66,6 +76,77 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
     }
 
     void onVolumeAdjustment(int adjustment, int zoneId) {
+        if (isOemDuckingServiceAvailable()) {
+            evaluateVolumeAdjustmentExternal(adjustment, zoneId);
+            return;
+        }
+        evaluateVolumeAdjustmentInternal(adjustment, zoneId);
+    }
+
+    private void evaluateVolumeAdjustmentExternal(int adjustment, int zoneId) {
+        OemCarAudioVolumeRequest request = getCarAudioVolumeRequest(zoneId);
+
+        if (DEBUG) {
+            Slogf.d(TAG_AUDIO, "evaluateVolumeAdjustmentExternal %s", request);
+        }
+
+        OemCarVolumeChangeInfo changedVolume =
+                CarLocalServices.getService(CarOemProxyService.class)
+                        .getCarOemAudioVolumeService()
+                        .getSuggestedGroupForVolumeChange(request, adjustment);
+
+        if (changedVolume == null || !changedVolume.isVolumeChanged()) {
+            if (DEBUG) {
+                Slogf.d(TAG_AUDIO, "No volume change for adjustment %s in zone %s",
+                        adjustToString(adjustment), zoneId);
+            }
+            return;
+        }
+
+        CarVolumeGroupInfo info = changedVolume.getChangedVolumeGroup();
+        int flags = FLAG_FROM_KEY | FLAG_SHOW_UI;
+
+        switch (adjustment) {
+            case ADJUST_LOWER: // Fallthrough
+            case ADJUST_RAISE:
+                mVolumeCallback.onGroupVolumeChange(info.getZoneId(), info.getId(),
+                        info.getVolumeGainIndex(), flags);
+                break;
+            case ADJUST_MUTE: // Fallthrough
+            case ADJUST_UNMUTE: // Fallthrough
+            case ADJUST_TOGGLE_MUTE:
+                mVolumeCallback.onMuteChange(info.isMuted(), info.getZoneId(), info.getId(), flags);
+                break;
+            case ADJUST_SAME: // Fallthrough
+            default:
+                // There should be a change in volume info, compare against the current one
+                CarVolumeGroupInfo currentInfo =
+                        mCarVolumeInfo.getVolumeGroupInfo(info.getZoneId(), info.getId());
+                if (info.isMuted() != currentInfo.isMuted()) {
+                    mVolumeCallback.onMuteChange(info.isMuted(), info.getZoneId(),
+                            info.getId(), flags);
+                }
+                if (info.getVolumeGainIndex() != currentInfo.getVolumeGainIndex()) {
+                    mVolumeCallback.onGroupVolumeChange(info.getZoneId(),
+                            info.getId(), info.getVolumeGainIndex(), flags);
+                }
+                break;
+        }
+    }
+
+    private OemCarAudioVolumeRequest getCarAudioVolumeRequest(int zoneId) {
+        List<CarVolumeGroupInfo> infos = mCarVolumeInfo.getVolumeGroupInfosForZone(zoneId);
+        List<AudioAttributes> activeAudioAttributes =
+                mCarVolumeInfo.getActiveAudioAttributesForZone(zoneId);
+
+        return new OemCarAudioVolumeRequest.Builder(zoneId)
+                .setCarVolumeGroupInfos(infos)
+                .setActivePlaybackAttributes(activeAudioAttributes)
+                .setCallState(mCarVolumeInfo.getCallStateForZone(zoneId))
+                .build();
+    }
+
+    private void evaluateVolumeAdjustmentInternal(int adjustment, int zoneId) {
         int groupId = mCarVolumeInfo.getVolumeGroupIdForAudioZone(zoneId);
         boolean isMuted = isMuted(zoneId, groupId);
 
@@ -118,6 +199,14 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
             return mCarVolumeInfo.isVolumeGroupMuted(zoneId, groupId);
         }
         return isMasterMute(mAudioManager);
+    }
+
+    private boolean isOemDuckingServiceAvailable() {
+        CarOemProxyService carService = CarLocalServices.getService(CarOemProxyService.class);
+
+        return carService != null
+                && carService.isOemServiceEnabled() && carService.isOemServiceReady()
+                && carService.getCarOemAudioVolumeService() != null;
     }
 
     public interface AudioPolicyVolumeCallbackInternal {

service/src/com/android/car/audio/CarAudioService.java
@@ -1095,7 +1095,7 @@ public class CarAudioService extends ICarAudio.Stub implements CarServiceBase {
      * returns the current call state ({@code CALL_STATE_OFFHOOK}, {@code CALL_STATE_RINGING},
      * {@code CALL_STATE_IDLE}) from the telephony manager.
      */
-    private int getCallStateForZone(int zoneId) {
+    int getCallStateForZone(int zoneId) {
         synchronized (mImplLock) {
             // Only driver can use telephony stack
             if (getUserIdForZoneLocked(zoneId) == mOccupantZoneService.getDriverUserId()) {
@@ -1756,6 +1756,14 @@ public class CarAudioService extends ICarAudio.Stub implements CarServiceBase {
         }
 
         return mutedGroups;
+    }
+
+    List<AudioAttributes> getActiveAudioAttributesForZone(int zoneId) {
+        List<AudioAttributes> activeAudioAttributes = new ArrayList<>();
+        activeAudioAttributes.addAll(getAllActiveAttributesForZone(zoneId));
+        activeAudioAttributes.addAll(getActiveHalAudioAttributesForZone(zoneId));
+
+        return activeAudioAttributes;
     }
 
     static final class SystemClockWrapper {

tests/carservice_unit_test/src/com/android/car/audio/CarAudioServiceUnitTest.java
@@ -114,6 +114,7 @@ import android.media.AudioFocusInfo;
 import android.media.AudioGain;
 import android.media.AudioManager;
 import android.media.AudioManager.AudioPlaybackCallback;
+import android.media.AudioPlaybackConfiguration;
 import android.media.IAudioService;
 import android.media.audiopolicy.AudioPolicy;
 import android.net.Uri;
@@ -136,6 +137,7 @@ import com.android.car.audio.hal.AudioControlWrapper;
 import com.android.car.audio.hal.AudioControlWrapper.AudioControlDeathRecipient;
 import com.android.car.audio.hal.AudioControlWrapperAidl;
 import com.android.car.audio.hal.HalAudioGainCallback;
+import com.android.car.audio.hal.HalFocusListener;
 import com.android.car.oem.CarOemProxyService;
 import com.android.car.test.utils.TemporaryFile;
 
@@ -260,6 +262,7 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     private AudioDeviceInfo mMicrophoneInputDevice;
     private AudioDeviceInfo mFmTunerInputDevice;
     private AudioDeviceInfo mMediaOutputDevice;
+
     @Captor
     private ArgumentCaptor<BroadcastReceiver> mVolumeReceiverCaptor;
 
@@ -332,7 +335,7 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     }
 
     private void setupService() throws Exception {
-        when(mMockContext.getSystemService(Context.TELECOM_SERVICE))
+        when(mMockContext.getSystemService(Context.TELEPHONY_SERVICE))
                 .thenReturn(mMockTelephonyManager);
         when(mMockContext.getPackageManager()).thenReturn(mMockPackageManager);
         doReturn(true)
@@ -2117,6 +2120,80 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
                 .isEqualTo(TEST_GAIN_INDEX);
     }
 
+    @Test
+    public void getActiveAudioAttributesForZone() {
+        mCarAudioService.init();
+
+        expectWithMessage("Default active audio attributes").that(
+                mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE)).isEmpty();
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActiveHalFocus() {
+        when(mAudioManager.requestAudioFocus(any())).thenReturn(
+                AudioManager.AUDIOFOCUS_REQUEST_GRANTED);
+        mCarAudioService.init();
+        requestHalAudioFocus(USAGE_ALARM);
+
+        expectWithMessage("HAL active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_ALARM).build());
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActivePlayback() {
+        mCarAudioService.init();
+        mockActivePlayback();
+
+        expectWithMessage("Playback active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_MEDIA).build());
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActiveHalAndPlayback() {
+        mCarAudioService.init();
+        mockActivePlayback();
+        when(mAudioManager.requestAudioFocus(any())).thenReturn(
+                AudioManager.AUDIOFOCUS_REQUEST_GRANTED);
+        requestHalAudioFocus(USAGE_VOICE_COMMUNICATION);
+
+        expectWithMessage("Playback active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_MEDIA).build(),
+                        new AudioAttributes.Builder().setUsage(USAGE_VOICE_COMMUNICATION).build());
+    }
+
+    @Test
+    public void getCallStateForZone_forPrimaryZone() throws Exception {
+        when(mMockTelephonyManager.getCallState()).thenReturn(TelephonyManager.CALL_STATE_OFFHOOK);
+        when(mMockOccupantZoneService.getDriverUserId()).thenReturn(TEST_DRIVER_USER_ID);
+        mCarAudioService.init();
+        when(mMockOccupantZoneService.getUserForOccupant(anyInt()))
+                .thenReturn(TEST_DRIVER_USER_ID, TEST_USER_ID_SECONDARY);
+        ICarOccupantZoneCallback callback = getOccupantZoneCallback();
+        callback.onOccupantZoneConfigChanged(CarOccupantZoneManager.ZONE_CONFIG_CHANGE_FLAG_USER);
+
+        expectWithMessage("Primary zone call state").that(
+                mCarAudioService.getCallStateForZone(PRIMARY_AUDIO_ZONE))
+                .isEqualTo(TelephonyManager.CALL_STATE_OFFHOOK);
+    }
+
+    @Test
+    public void getCallStateForZone_forNonPrimaryZone() throws Exception {
+        when(mMockTelephonyManager.getCallState()).thenReturn(TelephonyManager.CALL_STATE_OFFHOOK);
+        when(mMockOccupantZoneService.getDriverUserId()).thenReturn(TEST_DRIVER_USER_ID);
+        mCarAudioService.init();
+        when(mMockOccupantZoneService.getUserForOccupant(anyInt()))
+                .thenReturn(TEST_USER_ID, TEST_USER_ID_SECONDARY);
+        ICarOccupantZoneCallback callback = getOccupantZoneCallback();
+        callback.onOccupantZoneConfigChanged(CarOccupantZoneManager.ZONE_CONFIG_CHANGE_FLAG_USER);
+
+        expectWithMessage("Secondary zone call state").that(
+                        mCarAudioService.getCallStateForZone(SECONDARY_ZONE_ID))
+                .isEqualTo(TelephonyManager.CALL_STATE_IDLE);
+    }
+
     private CarAudioGainConfigInfo createCarAudioGainConfigInfo(int zoneId,
             String devicePortAddress, int volumeIndex) {
         AudioGainConfigInfo configInfo = new AudioGainConfigInfo();
@@ -2145,6 +2222,31 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
                 ArgumentCaptor.forClass(CarInputService.KeyEventListener.class);
         verify(mMockCarInputService).registerKeyEventListener(captor.capture(), any());
         return captor.getValue();
+    }
+
+    private void requestHalAudioFocus(int usage) {
+        ArgumentCaptor<HalFocusListener> captor =
+                ArgumentCaptor.forClass(HalFocusListener.class);
+        verify(mAudioControlWrapperAidl).registerFocusListener(captor.capture());
+        HalFocusListener halFocusListener = captor.getValue();
+        halFocusListener.requestAudioFocus(usage, PRIMARY_AUDIO_ZONE,
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+    }
+
+    private void mockActivePlayback() {
+        AudioPlaybackCallback callback = getCarAudioPlaybackCallback();
+        callback.onPlaybackConfigChanged(List.of(getPlaybackConfig()));
+    }
+
+    private AudioPlaybackConfiguration getPlaybackConfig() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(USAGE_MEDIA).build();
+        AudioPlaybackConfiguration config = mock(AudioPlaybackConfiguration.class);
+        when(config.getAudioAttributes()).thenReturn(audioAttributes);
+        when(config.getAudioDeviceInfo()).thenReturn(mMediaOutputDevice);
+        when(config.isActive()).thenReturn(true);
+
+        return config;
     }
 
     private void mockGrantCarControlAudioSettingsPermission() {

bluestacks/packages-services-Car-a16

Merged-In: Ia158c41525d7b58d37b23f012c45e132d0da5e9f
(cherry picked from commit 9e3dab244d6f1b39b499d080ea46e1f18b5ec9d1)

service/src/com/android/car/audio/CarAudioPolicyVolumeCallback.java
@@ -27,22 +27,32 @@ import static android.media.AudioManager.ADJUST_TOGGLE_MUTE;
 import static android.media.AudioManager.ADJUST_UNMUTE;
 import static android.media.AudioManager.FLAG_FROM_KEY;
 import static android.media.AudioManager.FLAG_SHOW_UI;
-import static android.util.Log.DEBUG;
 import static android.util.Log.VERBOSE;
 
 import static com.android.car.CarLog.TAG_AUDIO;
 
 import android.annotation.NonNull;
 import android.car.builtin.util.Slogf;
+import android.car.media.CarVolumeGroupInfo;
+import android.car.oem.OemCarAudioVolumeRequest;
+import android.car.oem.OemCarVolumeChangeInfo;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.audiopolicy.AudioPolicy;
 import android.media.audiopolicy.AudioPolicy.Builder;
+import android.util.Log;
 
+import com.android.car.CarLocalServices;
+import com.android.car.oem.CarOemProxyService;
 import com.android.internal.annotations.VisibleForTesting;
 
+import java.util.List;
 import java.util.Objects;
 
-final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCallback{
+final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCallback {
+
+    public static final boolean DEBUG = Slogf.isLoggable(TAG_AUDIO, Log.DEBUG);
+
     private final AudioManager mAudioManager;
     private final boolean mUseCarVolumeGroupMuting;
     private final CarVolumeInfoWrapper mCarVolumeInfo;
@@ -54,7 +64,7 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
         policyBuilder.setAudioPolicyVolumeCallback(
                 new CarAudioPolicyVolumeCallback(audioManager, carVolumeInfo,
                         useCarVolumeGroupMuting));
-        if (Slogf.isLoggable(TAG_AUDIO, DEBUG)) {
+        if (DEBUG) {
             Slogf.d(TAG_AUDIO, "Registered car audio policy volume callback");
         }
     }
@@ -70,8 +80,79 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
 
     @Override
     public void onVolumeAdjustment(int adjustment) {
+        if (isOemDuckingServiceAvailable()) {
+            evaluateVolumeAdjustmentExternal(adjustment, PRIMARY_AUDIO_ZONE);
+            return;
+        }
+        evaluateVolumeAdjustmentForPrimaryZoneInternal(adjustment);
+    }
+
+    private void evaluateVolumeAdjustmentExternal(int adjustment, int zoneId) {
+        OemCarAudioVolumeRequest request = getCarAudioVolumeRequest(zoneId);
+
+        if (DEBUG) {
+            Slogf.d(TAG_AUDIO, "evaluateVolumeAdjustmentExternal %s", request);
+        }
+
+        OemCarVolumeChangeInfo changedVolume =
+                CarLocalServices.getService(CarOemProxyService.class)
+                        .getCarOemAudioVolumeService()
+                        .getSuggestedGroupForVolumeChange(request, adjustment);
+
+        if (changedVolume == null || !changedVolume.isVolumeChanged()) {
+            if (DEBUG) {
+                Slogf.d(TAG_AUDIO, "No volume change for adjustment %s in zone %s",
+                        adjustToString(adjustment), zoneId);
+            }
+            return;
+        }
+
+        CarVolumeGroupInfo info = changedVolume.getChangedVolumeGroup();
+        int flags = FLAG_FROM_KEY | FLAG_SHOW_UI;
+
+        switch (adjustment) {
+            case ADJUST_LOWER: // Fallthrough
+            case ADJUST_RAISE:
+                mCarVolumeInfo.setGroupVolume(info.getZoneId(), info.getId(),
+                        info.getVolumeGainIndex(), flags);
+                break;
+            case ADJUST_MUTE: // Fallthrough
+            case ADJUST_UNMUTE: // Fallthrough
+            case ADJUST_TOGGLE_MUTE:
+                setMute(info.isMuted(), info.getId(), flags);
+                break;
+            case ADJUST_SAME: // Fallthrough
+            default:
+                // There should be a change in volume info, compare against the current one
+                CarVolumeGroupInfo currentInfo =
+                        mCarVolumeInfo.getVolumeGroupInfo(info.getZoneId(), info.getId());
+                if (info.isMuted() != currentInfo.isMuted()) {
+                    setMute(info.isMuted(), info.getId(), flags);
+                }
+                if (info.getVolumeGainIndex() != currentInfo.getVolumeGainIndex()) {
+                    mCarVolumeInfo.setGroupVolume(info.getZoneId(),
+                            info.getId(), info.getVolumeGainIndex(), flags);
+                }
+                break;
+        }
+    }
+
+    private OemCarAudioVolumeRequest getCarAudioVolumeRequest(int zoneId) {
+        List<CarVolumeGroupInfo> infos = mCarVolumeInfo.getVolumeGroupInfosForZone(zoneId);
+        List<AudioAttributes> activeAudioAttributes =
+                mCarVolumeInfo.getActiveAudioAttributesForAudioZone(zoneId);
+
+        return new OemCarAudioVolumeRequest.Builder(zoneId)
+                .setCarVolumeGroupInfos(infos)
+                .setActivePlaybackAttributes(activeAudioAttributes)
+                .setCallState(mCarVolumeInfo.getCallStateForZone(zoneId))
+                .build();
+    }
+
+    private void evaluateVolumeAdjustmentForPrimaryZoneInternal(int adjustment) {
+        // We only deal with primary zone as key event are not handle else where
         int zoneId = PRIMARY_AUDIO_ZONE;
-        int groupId = mCarVolumeInfo.getVolumeGroupIdForAudioZone(zoneId);
+        int groupId = mCarVolumeInfo.getVolumeGroupIdForPrimaryZone();
         boolean isMuted = isMuted(zoneId, groupId);
 
         if (Slogf.isLoggable(TAG_AUDIO, VERBOSE)) {
@@ -126,5 +207,13 @@ final class CarAudioPolicyVolumeCallback extends AudioPolicy.AudioPolicyVolumeCa
             return;
         }
         mCarVolumeInfo.setMasterMute(mute, flags);
+    }
+
+    private boolean isOemDuckingServiceAvailable() {
+        CarOemProxyService carService = CarLocalServices.getService(CarOemProxyService.class);
+
+        return carService != null
+                && carService.isOemServiceEnabled() && carService.isOemServiceReady()
+                && carService.getCarOemAudioVolumeService() != null;
     }
 }

service/src/com/android/car/audio/CarAudioService.java
@@ -1008,7 +1008,7 @@ public class CarAudioService extends ICarAudio.Stub implements CarServiceBase {
      * returns the current call state ({@code CALL_STATE_OFFHOOK}, {@code CALL_STATE_RINGING},
      * {@code CALL_STATE_IDLE}) from the telephony manager.
      */
-    private int getCallStateForZone(int zoneId) {
+    int getCallStateForZone(int zoneId) {
         synchronized (mImplLock) {
             // Only driver can use telephony stack
             if (getUserIdForZoneLocked(zoneId) == mOccupantZoneService.getDriverUserId()) {
@@ -1664,6 +1664,14 @@ public class CarAudioService extends ICarAudio.Stub implements CarServiceBase {
         }
 
         return mutedGroups;
+    }
+
+    List<AudioAttributes> getActiveAudioAttributesForZone(int zoneId) {
+        List<AudioAttributes> activeAudioAttributes = new ArrayList<>();
+        activeAudioAttributes.addAll(getActiveAttributesFromPlaybackConfigurations(zoneId));
+        activeAudioAttributes.addAll(getActiveHalAudioAttributesForZone(zoneId));
+
+        return activeAudioAttributes;
     }
 
     static final class SystemClockWrapper {

tests/carservice_unit_test/src/com/android/car/audio/CarAudioServiceUnitTest.java
@@ -90,6 +90,7 @@ import android.media.AudioDeviceInfo;
 import android.media.AudioFocusInfo;
 import android.media.AudioGain;
 import android.media.AudioManager;
+import android.media.AudioPlaybackConfiguration;
 import android.media.IAudioService;
 import android.media.audiopolicy.AudioPolicy;
 import android.net.Uri;
@@ -106,7 +107,9 @@ import com.android.car.CarLocalServices;
 import com.android.car.CarOccupantZoneService;
 import com.android.car.R;
 import com.android.car.audio.hal.AudioControlFactory;
+import com.android.car.audio.hal.AudioControlWrapper;
 import com.android.car.audio.hal.AudioControlWrapperAidl;
+import com.android.car.audio.hal.HalFocusListener;
 import com.android.car.oem.CarOemProxyService;
 import com.android.car.test.utils.TemporaryFile;
 
@@ -114,6 +117,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -200,6 +204,8 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     private Uri mNavSettingUri;
     @Mock
     private CarVolumeCallbackHandler mCarVolumeCallbackHandler;
+    @Mock
+    private AudioControlWrapperAidl mAudioControlWrapperAidl;
 
     private boolean mPersistMasterMute = true;
     private boolean mUseDynamicRouting = true;
@@ -209,7 +215,6 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     private TemporaryFile mTemporaryAudioConfigurationFile;
     private TemporaryFile mTemporaryAudioConfigurationWithoutZoneMappingFile;
     private Context mContext;
-    private AudioControlWrapperAidl mAudioControlWrapperAidl;
     private AudioDeviceInfo mTunerDevice;
     private AudioDeviceInfo mMediaOutputDevice;
 
@@ -269,13 +274,20 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     private void setupAudioControlHAL() {
         when(mBinder.queryLocalInterface(anyString())).thenReturn(mAudioControl);
         doReturn(mBinder).when(AudioControlWrapperAidl::getService);
-        mAudioControlWrapperAidl = new AudioControlWrapperAidl(mBinder);
+        when(mAudioControlWrapperAidl.supportsFeature(
+                AudioControlWrapper.AUDIOCONTROL_FEATURE_AUDIO_DUCKING)).thenReturn(true);
+        when(mAudioControlWrapperAidl.supportsFeature(
+                AudioControlWrapper.AUDIOCONTROL_FEATURE_AUDIO_FOCUS)).thenReturn(true);
+        when(mAudioControlWrapperAidl.supportsFeature(
+                AudioControlWrapper.AUDIOCONTROL_FEATURE_AUDIO_GAIN_CALLBACK)).thenReturn(true);
+        when(mAudioControlWrapperAidl.supportsFeature(
+                AudioControlWrapper.AUDIOCONTROL_FEATURE_AUDIO_GROUP_MUTING)).thenReturn(true);
         doReturn(mAudioControlWrapperAidl)
                 .when(() -> AudioControlFactory.newAudioControl());
     }
 
     private void setupService() throws Exception {
-        when(mMockContext.getSystemService(Context.TELECOM_SERVICE))
+        when(mMockContext.getSystemService(Context.TELEPHONY_SERVICE))
                 .thenReturn(mMockTelephonyManager);
         when(mMockContext.getPackageManager()).thenReturn(mMockPackageManager);
         doReturn(true)
@@ -1358,6 +1370,76 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
 
         assertWithMessage("Volume group audio attributes with dynamic routing disabled")
                 .that(audioAttributes).isEmpty();
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone() {
+        mCarAudioService.init();
+
+        assertWithMessage("Default active audio attributes").that(
+                mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE)).isEmpty();
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActiveHalFocus() {
+        when(mAudioManager.requestAudioFocus(any())).thenReturn(
+                AudioManager.AUDIOFOCUS_REQUEST_GRANTED);
+        mCarAudioService.init();
+        requestHalAudioFocus(USAGE_ALARM);
+
+        assertWithMessage("HAL active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_ALARM).build());
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActivePlayback() {
+        mCarAudioService.init();
+        mockActivePlayback();
+
+        assertWithMessage("Playback active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_MEDIA).build());
+    }
+
+    @Test
+    public void getActiveAudioAttributesForZone_withActiveHalAndPlayback() {
+        mCarAudioService.init();
+        mockActivePlayback();
+        when(mAudioManager.requestAudioFocus(any())).thenReturn(
+                AudioManager.AUDIOFOCUS_REQUEST_GRANTED);
+        requestHalAudioFocus(USAGE_VOICE_COMMUNICATION);
+
+        assertWithMessage("Playback active audio attributes")
+                .that(mCarAudioService.getActiveAudioAttributesForZone(PRIMARY_AUDIO_ZONE))
+                .containsExactly(new AudioAttributes.Builder().setUsage(USAGE_MEDIA).build(),
+                        new AudioAttributes.Builder().setUsage(USAGE_VOICE_COMMUNICATION).build());
+    }
+
+    private void requestHalAudioFocus(int usage) {
+        ArgumentCaptor<HalFocusListener> captor =
+                ArgumentCaptor.forClass(HalFocusListener.class);
+        verify(mAudioControlWrapperAidl).registerFocusListener(captor.capture());
+        HalFocusListener halFocusListener = captor.getValue();
+        halFocusListener.requestAudioFocus(usage, PRIMARY_AUDIO_ZONE,
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+    }
+
+    private void mockActivePlayback() {
+        List<AudioPlaybackConfiguration> configurations = List.of(getPlaybackConfig());
+        when(mAudioManager.getActivePlaybackConfigurations())
+                .thenReturn(configurations);
+    }
+
+    private AudioPlaybackConfiguration getPlaybackConfig() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(USAGE_MEDIA).build();
+        AudioPlaybackConfiguration config = mock(AudioPlaybackConfiguration.class);
+        when(config.getAudioAttributes()).thenReturn(audioAttributes);
+        when(config.getAudioDeviceInfo()).thenReturn(mMediaOutputDevice);
+        when(config.isActive()).thenReturn(true);
+
+        return config;
     }
 
     private void mockGrantCarControlAudioSettingsPermission() {